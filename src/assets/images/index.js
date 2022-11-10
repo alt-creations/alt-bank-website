f3161b54f6e97175634bfdd366ba0af6c739fbea
Emmy Kuyele <emmykuyele@outlook.com>
2022-11-10 13:30:03 +0200

    Expose project

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,5 @@
-import airbnb from "./airbnb.png";
 import bill from "./bill.png";
-import binance from "./binance.png";
 import card from "./card.png";
-import coinbase from "./coinbase.png";
 import dropbox from "./dropbox.png";
 import logo from "./trans_logo.svg";
 import quotes from "./quotes.svg";
@@ -25,11 +22,8 @@ import people02 from "./people02.png";
 import people03 from "./people03.png";
 
 export {
-  airbnb,
   bill,
-  binance,
   card,
-  coinbase,
   dropbox,
   logo,
   quotes,
